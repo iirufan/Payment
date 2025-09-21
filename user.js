9616f63b5261758234d829607537983b175e4a07
iirufan <42711386+iirufan@users.noreply.github.com>
2025-09-21 14:19:20 +0500

    Update user.js

--- user.js
+++ user.js
@@ -1,4 +1,4 @@
-econst users = [
+const users = [
   { username: "Leeli", name: "Leeli Lounge", password: "Welcome@1234", rcNo: "1", WP: "Leeli", Level: "Supervisor" },
   { username: "Koveli", name: "Koveli Lounge", password: "12345", rcNo: "2", WP: "Koveli", Level: "User" }
 ];
